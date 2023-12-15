26965e362a9e90c1f59df4d69de7b4928ee0e02d
ss37939 <ss37939@georgiasouthern.edu>
2023-12-15 02:10:06 -0500

    First Commit
    This is the code so far. I may make changes later to up the difficulty and points.

---
+++ QuizGame.py
@@ -0,0 +1,47 @@
+gamePlay = True
+while gamePlay == True:
+    score = 0
+    print("Welcome to my computer quiz!")
+
+    #question1
+    answer = input("What does CPU stand for? ")
+    if answer.lower() == "central processing unit":
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+    #question2
+    answer = input("What does GPU stand for? ")
+    if answer.lower() == "graphics processing unit":
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+    #question3
+    answer = input("What does RAM stand for? ")
+    if answer.lower() == "random access memory":
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+    #question4
+    answer = input("What does PSU stand for? ")
+    if answer.lower() == "power supply unit":
+        print("Correct")
+        score += 1
+    else:
+        print("Incorrect")
+
+    print("You got " + str(score) + " questions correct!")
+    print("You got " + str((score / 4) * 100) + "%.")
+
+
+    playing = input("Would you like to play again? ")
+    if playing.lower() != "yes":
+        quit()
+    
+    print("Okay! Let's Play Then")
+   
